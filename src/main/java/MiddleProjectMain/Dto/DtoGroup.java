f670b45855739307209ed5e65c94f18170db15c6
DarkIL1ght <Darhansemyatov1@gmail.com>
2025-11-28 14:39:48 +0500

    correct dto

--- src/main/java/MiddleProjectMain/Dto/DtoGroup.java
+++ src/main/java/MiddleProjectMain/Dto/DtoGroup.java
@@ -4,9 +4,6 @@ import lombok.*;
 
 
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class DtoGroup {
     private Long id;
     private String name;
--- src/main/java/MiddleProjectMain/Dto/DtoStudent.java
+++ src/main/java/MiddleProjectMain/Dto/DtoStudent.java
@@ -3,17 +3,10 @@ package MiddleProjectMain.Dto;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class DtoStudent {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "t_name")
     private String name;
-    @Column(name = "t_surname")
     private String surname;
     private int age;
 
